a0503ea75e22938ed93628539706f143a6a1fa70
faizulislamfair <faizulislamfair@gmail.com>
2022-11-10 17:01:40 +0600

    reviews query

--- index.js
+++ index.js
@@ -58,6 +58,19 @@ async function run() {
         })
 
 
+        app.get('/reviews', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+
+
 
         app.post('/services', async (req, res) => {
             const service = req.body;
